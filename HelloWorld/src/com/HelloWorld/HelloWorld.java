d74a0a20d7eefcbf5b9c21776b16940295209159
number200203 <102226379+number200203@users.noreply.github.com>
2022-03-30 15:49:41 +0800

    Add files via upload

---
+++ HelloWorld/src/com/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Ye");
+		System.out.println("Use Internet to learn");
+	}
+}
